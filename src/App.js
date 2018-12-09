5ff2803e9e680664e96ccf7a483d324e8f174689
ojepse <ojepse@techdriven.org>
2018-12-09 11:11:48 -0600

    Changed app text in feature branch.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Holla back!
+           あのう。。。日本語が分かりますか？もしそうだったら、こんにちは！これ、初めて自分が作ったサイトなんです。
           </p>
           <a
             className="App-link"
